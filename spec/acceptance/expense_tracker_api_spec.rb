921e044c648e1991637c5ad7dc55d33b9805f116
Toan Nguyen <ntuantoan@gmail.com>
2018-05-22 07:44:29 +0700

    refactored, added pending test: get expenses by date

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -10,20 +10,43 @@ module ExpenseTracker
       ExpenseTracker::API.new
     end
 
-    it "records submitted expenses" do
-      coffee = {
-        payee:  "Starbucks",
-        amount: 5.75,
-        date:   "2017-06-10"
-      }
-
-      post "/expenses", JSON.generate(coffee)
-
+    def post_expense expense
+      post "/expenses", JSON.generate(expense)
       expect(last_response.status).to eq(200)
 
       response = JSON.parse last_response.body
-
       expect(response).to include("expense_id" => a_kind_of(Integer))
+
+      expense.merge "id" => response["expense_id"]
+    end
+
+    it "records submitted expenses" do
+      pending "need to persist expenses"
+      # POST coffee, zoo, and groceries expenses
+      coffee = post_expense(
+        "payee"  => "Starbucks",
+        "amount" => 5.75,
+        "date"   => "2017-06-10"
+      )
+
+      zoo = post_expense(
+        "payee"  => "Zoo",
+        "amount" => 15.25,
+        "date"   => "2017-06-10"
+      )
+
+      groceries = post_expense(
+        "payee"  => "Whole food",
+        "amount" => 95.20,
+        "date"   => "2017-06-11"
+      )
+
+      # GET expenses by date
+      get "expenses/2017-06-10"
+      expect(last_response.status).to eq(200)
+
+      response = JSON.parse last_response.body
+      expect(response).to contain_exactly(coffee, zoo)
     end
   end
 end
