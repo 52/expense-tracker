1a031e3cc6f9fb74c38ac15101da64a0dbc0e791
Toan Nguyen <ntuantoan@gmail.com>
2018-05-26 15:52:44 +0700

    Unit test for GET expenses/:date

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -2,7 +2,6 @@ require "rack/test"
 require_relative "../../../app/api"
 
 module ExpenseTracker
-
   RSpec.describe API do
     include Rack::Test::Methods
 
@@ -54,6 +53,48 @@ module ExpenseTracker
           post "/expenses", JSON.generate(expense)
 
           expect_response include("error_message" => "Expense incomplete")
+        end
+      end
+    end
+
+    describe "GET /expenses/:date" do
+      context "when expenses exist on the given date" do
+        let(:date){"2017-06-10"}
+
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with(date)
+            .and_return(["expense_1", "expense_2"])
+        end
+
+        it "responses with a 200 (OK)" do
+          get "expenses/#{date}"
+          expect(last_response.status).to eq(200)
+        end
+
+        it "returns the expenses record as JSON" do
+          get "expenses/#{date}"
+          expect_response contain_exactly("expense_1", "expense_2")
+        end
+      end
+
+      context "when there are no expense on the given date" do
+        let(:date){"2017-06-10"}
+
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with(date)
+            .and_return([])
+        end
+
+        it "responses with a 200 (OK)" do
+          get "expenses/#{date}"
+          expect(last_response.status).to eq(200)
+        end
+
+        it "returns an empty array as JSON" do
+          get "expenses/#{date}"
+          expect_response eq([])
         end
       end
     end
